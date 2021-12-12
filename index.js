9de6fcd9d6f116430882fdb44ec023e15e0e6cf4
rashidov <rashidov.aydar@mail.ru>
2021-12-12 12:45:55 +0300

    init

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const cors = require('cors')
+const useSocket =  require('socket.io')
+const bodyParser = require('body-parser')
+const PORT = process.env.APP_PORT
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const rooms = new Map()
+
+app.post('/rooms', (req, res) => {
+  const {roomId} = req.body
+  if (!rooms.has(roomId)) {
+    rooms.set(roomId, new Map([
+      ['users', new Map()],
+      ['messages', []]
+    ]))
+  }
+  res.send()
+})
+
+const io = useSocket(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
+
+io.on('connection', async socket => {
+
+  socket.on('ROOM:JOIN', ({ roomId, userName}) => {
+    socket.join(roomId)
+    rooms.get(roomId).get('users').set(socket.id, userName);
+    const users = [...rooms.get(roomId).get('users').values()];
+    const messages = [...rooms.get(roomId).get('messages').values()];
+
+    socket.to(roomId).emit('ROOM:JOINED', users)
+    socket.emit('ROOM:JOINED', users)
+    socket.emit('ROOM:MESSAGES', messages)
+  })
+
+  socket.on('ROOM:NEW_MESSAGE', ({ roomId, userName, text}) => {
+    const msg = {userName, text}
+    rooms.get(roomId).get('messages').unshift(msg)
+    socket.to(roomId).emit('ROOM:NEW_MESSAGE', msg)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log('server is started! port ->', PORT)
+})
